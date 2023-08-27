16367899fb894ec54004acc325b24718dbf0e57f
LeChris <bk2mua@gmail.com>
2023-08-27 22:21:34 +0300

    add FullscreenVideoPlayer

---
+++ Orcs Losses/Background/FullscreenVideoPlayer.swift
@@ -0,0 +1,31 @@
+//
+//  FullscreenVideoPlayer.swift
+//  Orcs Losses
+//
+//  Created by Le Chris on 27.08.2023.
+//
+
+import SwiftUI
+import AVKit
+
+struct FullscreenVideoPlayer: View {
+    private var player: AVPlayer
+    
+    init(videoURL: URL) {
+        player = AVPlayer(url: videoURL)
+        player.play()
+    }
+    
+    var body: some View {
+        VideoPlayer(player: player)
+//        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            player.play()
+            NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: nil, queue: .main) { _ in
+                player.seek(to: .zero)
+                player.play()
+            }
+            
+        }
+    }
+}
