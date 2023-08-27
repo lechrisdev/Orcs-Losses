6480e9dd67992bc5a07e90a308740b07772d7b2d
LeChris <bk2mua@gmail.com>
2023-08-27 10:59:15 +0300

    Initial Commit

---
+++ Orcs Losses/Orcs_LossesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orcs_LossesApp.swift
+//  Orcs Losses
+//
+//  Created by Le Chris on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Orcs_LossesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
